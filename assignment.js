b579886eb1c112355b52956f0b8459141f14c4ec
mahidhasan96 <mahidhasan1122@gmail.com>
2020-07-17 21:00:14 +0600

    adding some items

--- assignment.js
+++ assignment.js
@@ -5,24 +5,31 @@ function feetToaMile(feet){
     var mile = feet / 5280;
     return mile;
 }
+var result = feetToaMile(42240);
+console.log(result);
+
 
 // woodcalculator
 
-function woodcaculator(chair,table){
+function woodcaculator(chair,table, bed){
     var chairtCount = chair * 12;
     var tableCount = table * 12;
-    var totalWoodCount = chairtCount + tableCount;
+    var bedCount =  bed * 12;
+    var totalWoodCount = chairtCount + tableCount + bedCount;
     return totalWoodCount;
 
 }
+var woodResult = woodcaculator(50, 80, 45);
+console.log(woodResult);
 
+// brickCaculator 
 
 // tinyFriend
 
-function tinyFriend(numbers){
-    var tiny = numbers(0);
-    for(var  i = 0; i < numbers.length; i++){
-        list = numbers[i];
+function tinyFriend(names){
+    var tiny = names(0);
+    for(var  i = 0; i < names.length; i++){
+        list = names[i];
         if(list.length < tiny.length){
             tiny = list;
         }
@@ -30,3 +37,5 @@ function tinyFriend(numbers){
     }
     return tiny;
 }
+var result = tinyFriend[("mahid", "hasan", "kamal", "ad")];
+console.log(result);
